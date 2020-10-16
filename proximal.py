91c57122ff558e741bdd7093c619b599e168f7c1
Viloleal <vicloren@ucm.es>
2020-10-16 10:58:24 +0200

    modified proximal.py and vcf_comparison_final.ipynb

--- proximal.py
+++ proximal.py
@@ -14,8 +14,8 @@ import subprocess
 
 #Extracting headers from vcfs
 
-vcfs = glob.glob("*vcf*")
-prefixes = [re.sub(r'_.*','',vcf) for vcf in vcfs]
+vcfs = glob.glob("*vcf.gz")
+prefixes = [re.sub(r'.sim.vcf.gz','',vcf) for vcf in vcfs]
 vcfs.sort()
 prefixes.sort()
 path = os.getcwd()
@@ -33,7 +33,7 @@ for (files,prefix) in zip (vcfs,prefixes):
 	file.write("List of files \n vcfs \n %s \n Prefix \n %s \n ------------------- \n" % (listToStr1,listToStr2))
 	file.close()
 	subprocess.call(['bash', '-c', 'bcftools view -h %s >> %s.header.txt' % (files, prefix)])
-	subprocess.call(['bash', '-c', 'bcftools view --no-header %s >> %s.nohead.vcf' % (files, prefix)])
+	subprocess.call(['bash', '-c', 'bcftools view --no-header %s -O v -o %s.nohead.vcf' % (files, prefix)])
 	for files in os.listdir(path):
 		if files.endswith("header.txt"):
 			shutil.copy(files,prox_dir)
@@ -55,25 +55,26 @@ for header in headers:
 #removing snps within 10 bp from each other
 
 vcfs = glob.glob("*nohead.vcf")
-prefixes=[re.sub(r'_.*','',vcf) for vcf in vcfs]
+prefixes=[re.sub(r'.nohead.vcf','',vcf) for vcf in vcfs]
 vcfs.sort()
 prefixes.sort()
 
-def prox(i):
-    rows = df2.shape[0]
-    while i < rows: #quito el igual
-    	y = i 
-    	if y == rows-1: #cambio y >= rows por y == rows-1 para que cuando el bucle entre en la ultima fila se pare (ya que no puedes añadir un campo diff en la siguiente, porque no existe, y aparece el error de "out of bounds") 
-    		pass
-    		i+=1 #sumo 1 para que finalice el bucle while una vez entremos en esta condición. Si no sumas 1 el while se convierte en un bucle infinito.
-    	else:
-	        y = i + 1
-	        di = df2.iloc[i,1]
-	        dy = df2.iloc[y,1]
-	        diff = np.subtract(dy, di)
-	        #print(diff)
-	        df2.loc[df2.index[y], 'DIFF'] = diff
-	        i = i + 1
+def prox(dist):
+    ex=[]
+    remove = pd.DataFrame(columns=["Pos1", "Pos2", "Pos3"])
+    for i in range(len(df2)-1):
+        #print (r.pos)
+        a=df2.loc[i].pos
+        b=df2.loc[i+1].pos
+        if b-a<=dist:
+            #print (a,b, a-b)
+            ex.extend([a,b])
+            d = pd.DataFrame({"Pos1":[a], "Pos2":[b], "Pos3":[b-a]})
+            remove = remove.append(d)
+    #print (ex)
+    return df2[~df2.pos.isin(ex)], remove
+
+
 
 path = os.getcwd()
 
@@ -84,22 +85,17 @@ for (files,prefix) in zip (vcfs,prefixes):
 	file = open("vcfs_prefixes1.txt", "a")
 	file.write("List of files \n vcfs \n %s \n Prefix \n %s \n ------------------- \n" % (listToStr1,listToStr2))
 	file.close()
-	df1 = pd.read_table('%s/%s' % (path,files), header = None) 
-	df2 = df1[df1[3] != 'N'] 
-	prox(0)
-	df10 = df2.loc[df2['DIFF'] <= 10]
-	df2 = df2.drop(df2[df2.DIFF <=10].index)
-	dfn = df1.loc[df1[3] == 'N']
-	df = pd.concat([df2,dfn])
-	df = df.drop(['DIFF'], axis=1)
-	df = df.sort_index(axis=0)
-	df.to_csv('%s_proc.vcf' % (prefix), sep = '\t', header=False, index=False)
-	df10.to_csv('%s_proc.10.vcf' % (prefix), sep = '\t', header=False, index=False)
+	df1 = pd.read_table('%s.nohead.vcf' % (prefix), names=['chrom','pos','a','b','c','d','e','f'], index_col=False)
+	df2 = df1[df1.b != 'N'] 
+	dfx, remove = prox(10)
+	print ("Removed %s positions" % (len(df2)-len(dfx)))
+	dfx.to_csv('%s_processed.vcf' % (prefix), sep = '\t', header=False, index=False)
+	remove.to_csv('%s_removed.vcf' % (prefix), header=True,index=False)
 
 #Changing headers to vcf files
 
-vcfs = glob.glob("*_proc.vcf")
-prefixes=[re.sub(r'_.*','',vcf) for vcf in vcfs]
+vcfs = glob.glob("*_processed.vcf")
+prefixes=[re.sub(r'.processed.vcf','',vcf) for vcf in vcfs]
 headers = glob.glob("*header.txt")
 vcfs.sort()
 prefixes.sort()
@@ -125,9 +121,9 @@ proc_dir = os.path.join(path, r'Processed_vcfs')
 os.mkdir(proc_dir)
 
 for files in os.listdir(path):
-	if files.endswith("processed_zc.vcf"):
+	if files.endswith("processed.vcf"):
 		shutil.move(files,proc_dir)
-	if files.endswith("_proc.10.vcf"):
+	if files.endswith("removed.vcf"):
 		shutil.move(files,rem_dir)
 for header in headers:
 	header_dir = os.path.join(path, '%s' % (header))
